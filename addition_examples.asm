c711ef870a218970224a546e2f3be44e0037e917
daucloudlab <daucloudlab@gmail.com>
2023-03-07 16:18:13 +0600

    added more add example

--- addition_examples.asm
+++ addition_examples.asm
@@ -1,4 +1,4 @@
-.data
+
 
 
 .text
@@ -22,4 +22,39 @@
 	# add $9, $10, $1
 	addi $t1, $t2, 5647123
 	
+
+	addi $v0, $zero, 4
 	
+	# lui $1, 0x00001001
+	# ori $1, $1, 0x00000000
+	la $a0, prompt1
+	syscall
+	
+	addi $v0, $zero, 5
+	syscall
+	move $t1, $v0
+	
+	addi $v0, $zero, 4
+	la $a0, prompt2
+	syscall
+	
+	addi $v0,$zero, 5
+	syscall
+	move $t2, $v0
+	
+	add $s0,$t1, $t2
+	
+	addi $v0, $zero, 4
+	la $a0, result
+	syscall
+	
+	addi $v0, $zero, 1
+	move $a0, $s0
+	syscall
+	  
+		
+.data
+	prompt1 : .asciiz "Please enter the first value to add: "
+	prompt2 : .asciiz "Please enter the second value to add: "
+	result : .asciiz "result is "
+		
